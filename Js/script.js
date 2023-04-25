7e60023ebebfe948d878f57c6505a9404d36ec97
Yuzhanin Daniel <yuznin@gmail.com>
2023-04-25 11:46:44 +0300

    Js location fix

---
+++ Js/script.js
@@ -0,0 +1,50 @@
+setTimeout(function () {
+    location.href = 'rocket.html'
+}, 2000);
+
+
+var doCoolStuff = function () {
+    var currentClassName = document.getElementById('cool').className;
+
+    if (currentClassName == 'cool') {
+        document.getElementById('cool').className = 'cool red';
+    }
+    else {
+        document.getElementById('cool').className = 'cool';
+    }
+}
+
+var sayMyName = function (name) {
+    alert('My name is ' + name);
+}
+
+var car = {
+    make: 'VW',
+    type: 'Polo',
+    color: 'Blue',
+    isTurenedOn: false,
+    numberOfWheels: 4,
+    seats: [
+        'seat1',
+        'seat2',
+        'seat3',
+        'seat4'
+    ],
+    tronOn: function () {
+        this.isTurenedOn = true;
+    },
+    fly: function () {
+        alert('fly');
+    },
+    switchCar: function (isOn) {
+        console.log('turn car ' + isOn);
+        if (isOn == true) {
+            this.isTurenedOn = true;
+        }
+        else {
+            this.isTurenedOn = false;
+        }
+    }
+};
+
+console.log('Hello there friends!');
